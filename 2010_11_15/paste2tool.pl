22e78e21f1f544ae40b144dd1a2d424a48d8ec82
EragonJ <jack.xxlong@gmail.com>
2010-11-17 00:12:02 +0800

    Add font directory and update paste2tool.pl
    Fix $ARGV index problem and detect platform automatically then store in
    the clipboard.

--- 2010_11_15/paste2tool.pl
+++ 2010_11_15/paste2tool.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl -w
-# I have to fix chinese words problems in $ARGV and try to utilize it with long sentences input
 use strict;
 use WWW::Mechanize;
 my $mech = WWW::Mechanize->new();
-my $code = $ARGV[1] || "test";
+my $platform = `uname`;
+my $code = $ARGV[0] || "test";
 my %postFields = (
   lang => "text",
   description => "",
@@ -11,5 +11,11 @@ my %postFields = (
   parent => 0
 );
 $mech->post("http://paste2.org/new-paste",\%postFields);
-my $link = $1 if ($mech->content() =~ m{<link rel='stylesheet'.*href='/style/(.*?).css'});
-print "http://paste2.org/p/$link\n";
+
+# Set the link and copy command
+my $link = "http://paste2.org/p/$1" if ($mech->content() =~ m{<link rel='stylesheet'.*href='/style/(.*?).css'});
+my $copy = ($platform =~ /Darwin/) ? "pbcopy" :  "xclip -selection clipboard";
+
+# Final , print the link and store it in the clipboard
+print $link;
+`echo $link | $copy`;
