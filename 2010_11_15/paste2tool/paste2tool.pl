31b6d77240e83e0a88634371f24dce6a9473fffc
EragonJ <jack.xxlong@gmail.com>
2010-11-19 22:51:14 +0800

    Add a mechanism that you can open files.

--- 2010_11_15/paste2tool/paste2tool.pl
+++ 2010_11_15/paste2tool/paste2tool.pl
@@ -2,8 +2,30 @@
 use strict;
 use WWW::Mechanize;
 my $mech = WWW::Mechanize->new();
-my $platform = `uname`;
-my $code = $ARGV[0] || "test";
+my $code;
+
+# It means we have many files to load
+if(scalar(@ARGV)>0)
+{
+  for my $filename (@ARGV)
+  {
+    # Open all files and store in $code
+    {
+      local $/ = undef; 
+      local *FILE; 
+      open FILE, "<$filename"; 
+      $code .= <FILE>."\n"; 
+      close FILE; 
+    }
+  }
+  $code .= "This file is generated automatically by paste2tool!";
+}
+else
+{
+  print "Type something by yourself.\n> ";
+  $code = <>;
+}
+
 my %postFields = (
   lang => "text",
   description => "",
@@ -12,10 +34,11 @@ my %postFields = (
 );
 $mech->post("http://paste2.org/new-paste",\%postFields);
 
+my $platform = `uname`;
 # Set the link and copy command
 my $link = "http://paste2.org/p/$1" if ($mech->content() =~ m{<link rel='stylesheet'.*href='/style/(.*?).css'});
 my $copy = ($platform =~ /Darwin/) ? "pbcopy" :  "xclip -selection clipboard";
 
 # Final , print the link and store it in the clipboard
-print $link;
+print "$link\n";
 `echo $link | $copy`;
